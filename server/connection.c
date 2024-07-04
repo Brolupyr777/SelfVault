92a05a4d5d378ab18cff4b80c9d0f41d2f8d9b0b
Brolupyr777 <bohdan.lupyr@gmail.com>
2024-07-03 17:50:54 -0700

    Partial Construction of Socket

--- server/connection.c
+++ server/connection.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 int main(int argc, char *argv[]) {
 
+  struct addrinfo info;
+  
   printf("Hello World\n");
+
+  getaddrinfo("192.168.1.21", "80",);
 
   return 0;
 }
