9d56bdf503e80266ec79fc05c2dca175d552bed2
Brolupyr777 <bohdan.lupyr@gmail.com>
2024-07-04 17:19:42 -0700

    Update

--- server/connection.c
+++ server/connection.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[]) {
+  struct sockaddr_in sin = {.sin_family = AF_INET,
+                            .sin_port = htons(4534),
+                            .sin_addr.s_addr = htons(192168121)};
 
+  // memset(sin.sin_zero, 0, sizeof(sin.sin_zero));
   struct addrinfo info;
-  
+
   printf("Hello World\n");
 
-  getaddrinfo("192.168.1.21", "80",);
+  // getaddrinfo("192.168.1.21", "80",);
 
   return 0;
 }
--- server/send.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-  return EXIT_SUCCESS;
-}
