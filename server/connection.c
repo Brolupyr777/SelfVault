0e09e6a1f63491253fc5f7ff2d02ccd3cb431c91
Bohdan Lupyr <bohdan.lupyr@gmail.com>
2024-07-09 13:58:59 -0700

    error checking addrinfo and freeing memory after use

--- server/connection.c
+++ server/connection.c
@@ -8,24 +8,40 @@
 int main(int argc, char *argv[]) {
   char ip6[INET6_ADDRSTRLEN] = "2601:1c2:4b00:5150::8a";
 
-  struct sockaddr_in6 sin = {
-      .sin6_family = AF_INET6,
-      .sin6_port = htons(4534),
-  };
+  // struct sockaddr_in6 sin = {
+  //     .sin6_family = AF_INET6,
+  //     .sin6_port = htons(4534),
+  // };
 
-  int address_fill = inet_pton(
-      AF_INET6, "192.168.1.21",
-      &(sin.sin6_addr)); // Function that allows to convert regular IP address
-                         // notation into the one that's needed for the struct
-  if (!address_fill)
-    return 1;
+  // int address_fill = inet_pton(
+  //     AF_INET6, "192.168.1.21",
+  //     &(sin.sin6_addr)); // Function that allows to convert regular IP address
+  //                        // notation into the one that's needed for the struct
+  // if (!address_fill)
+  //   return 1;
 
   // memset(sin.sin_zero, 0, sizeof(sin.sin_zero));
-  struct addrinfo info;
+
+  // Status of the connection
+  int status;
+
+  // Hints to be used for configuring socket connection
+  struct addrinfo hints;
+
+  // Struct pointer, that points to the result
+  struct addrinfo *servinfo;
+
+
 
   printf("Hello World\n");
 
-  // getaddrinfo("192.168.1.21", "80",);
+  // The function sets up the structs, not making the connection yet
+  if ((status = getaddrinfo("2601:1c2:4b00:5150::8a", "13929", &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
 
+    exit(1);
+  }
+  
+  freeaddrinfo(&servinfo);
   return 0;
 }
